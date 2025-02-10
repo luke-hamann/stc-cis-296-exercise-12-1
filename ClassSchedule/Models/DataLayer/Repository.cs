4971a33eea8a2ab4085c2c99e5ece99a07c0281b
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-02-10 09:57:36 -0600

    Add step 32.

--- ClassSchedule/Models/DataLayer/Repository.cs
+++ ClassSchedule/Models/DataLayer/Repository.cs
@@ -35,6 +35,18 @@ namespace ClassSchedule.Models
         }
 
         public virtual T Get(int id) => dbset.Find(id);
+
+        public virtual T Get(QueryOptions<T> options)
+        {
+            IQueryable<T> query = dbset;
+            foreach (string include in options.GetIncludes()) {
+                query = query.Include(include);
+            }
+            if (options.HasWhere)
+                query = query.Where(options.Where);
+            return query.FirstOrDefault();
+        }
+
         public virtual void Insert(T entity) => dbset.Add(entity);
         public virtual void Update(T entity) => dbset.Update(entity);
         public virtual void Delete(T entity) => dbset.Remove(entity);
